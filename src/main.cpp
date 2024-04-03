558149c5b970a7e69cf45a56d62dcf8b9f14c6ca
FinnMew <finngrayharvey@outlook.com>
2024-04-04 01:03:24 +1100

    feature signup working

--- src/main.cpp
+++ src/main.cpp
@@ -1,189 +1,134 @@
 #include <avr/io.h>
-#include <util/delay.h>
 #include <stdlib.h>
-#include <math.h>
-
+#include <util/delay.h>
 #include <avr/interrupt.h>
 
 #include "usart.h"
 #include "bit.h"
 
+#define pinPhotocell PC0 
+
+int readADC(unsigned char pin) ;
+void ledControl(float value_ADC) ;
+
 int setPrescaler_tc0(char option) ;
-void my_delay_us(unsigned long x) ;
 void set_tc0_mode(char mode) ;
 
-void my_delay_1e6us() ;
-void my_delay_2s_ctc() ;
-void my_delay_us_ctc(float x, unsigned char top, char prescaler_option) ;
+float value_ADC ;
 
-volatile unsigned long numOV = 0 ;
-
-ISR(TIMER0_OVF_vect) {
-    numOV++ ;
+ISR (ADC_vect) {
+    value_ADC = readADC(pinPhotocell) ;
+    bitSet(ADCSRA, ADSC) ;    // Start next conversion
 }
 
-ISR(TIMER0_COMPA_vect) {
-    numOV++ ;
-}
+int main () {
 
-ISR(TIMER0_COMPB_vect) {
-    numOV++ ;
-}
+    char PhotoResValArray[11] ;
+    usart_init(103) ;
+    bitSet(DDRD, PIND6) ;
 
-int main(void) {
+    bitSet(ADCSRA, ADPS2) ;   // prescalar
+    bitSet(ADCSRA, ADPS1) ;   // prescalar
+    bitSet(ADCSRA, ADPS0) ;   // prescalar
+    bitClear(ADMUX, ADLAR) ;  // setting 0 for 10 bit
+    bitSet(ADMUX, REFS0) ;    // Aref settings
+    bitSet(ADCSRA, ADIE) ;    // Enable intterupt
+    sei() ;                    
+    bitSet(ADCSRA, ADEN) ;    // Enables ADC
+    bitSet(ADCSRA, ADSC) ;    // Start conversion
 
-    usart_init(103) ; // set baud rate to 9600 : B = clock frequecny/16 x (ubrr +1) = 16000/16(104) = 9615.4
+    set_tc0_mode(3) ;
+    bitClear(TCCR0A, COM0A0) ;
+    bitClear(TCCR0A, COM0A1) ;
+    bitClear(TCCR0A, COM0B0) ;
+    bitClear(TCCR0A, COM0B1) ;
 
-    bitSet(DDRD, PIND6) ; // set PIND6 as output
-    bitSet(DDRD, PIND5) ; // set PIND5 as output
+    bitSet(TCCR0A, COM0A1) ;
+    bitSet(TCCR0A, COM0B1) ;
 
-    bitSet(TIMSK0, TOIE0) ; // timer/counter0 overflow interrupt enabled 
-    bitSet(TCCR0A, WGM01) ;// CTC mode
-    sei() ; // interupt enabled
+    setPrescaler_tc0(3) ;
 
     while (1) {
-        bitSet(PORTD, PIND6) ;   // turn PIND6 ON
-        usart_tx_string(">b:") ;
-        usart_transmit('1') ;
-        usart_transmit('\n') ;
-        my_delay_2s_ctc() ;
-        
-        bitSet(PORTD, PIND6) ;
-        usart_tx_string(">b:") ;
-        usart_transmit('0') ;
-        usart_transmit('\n') ;
-        my_delay_us_ctc(1.23, 124, 5) ;
 
+        dtostrf(value_ADC, 4, 3, PhotoResValArray) ;
+        PhotoResValArray[11] = '\n' ;
+
+        usart_tx_string(">a") ;
+        usart_tx_string(PhotoResValArray) ;
+        usart_transmit('\n') ;
+
+       ledControl(value_ADC) ;
     }
-    return 0 ;
+} 
+
+int readADC(unsigned char pin) {
+    int volt = ADCL ;
+    volt |= (ADCH << 8) ;
+    return volt ;
 }
 
+void ledControl(float value_ADC) {
 
-void my_delay_1e6us() {
-    
-    set_tc0_mode(0) ; // set normal mode
-    bitSet(TIMSK0, TOIE0) ; // timer/counter0 overflow interrupt enable
+    float voltage = (value_ADC * 5) / 1023 ;
+    float resistance = (1000 * (5 - voltage)) / voltage ;
 
-    unsigned long numOV_max = 62500 ; // for 1 second delay overflows needed = (time of delay) / (256 x T) = 1/(256/16000000) = 62500
-
-    numOV = 0 ; // overflows count
-    TCNT0 = 0 ; // timer starts at 0
-    bitSet(TCCR0B, CS00) ; // set no prescalar
-    while (numOV < numOV_max - 1) ; // wait until overflows reaches required amount, 62500 in this case for 1s
-    bitClear(TCCR0B, CS00) ; // clear CS00, stopping timer0
+    OCR0A = resistance / 255 ;
 }
-
 
 int setPrescaler_tc0(char option) {
-
-    if(option == 0) {
-        bitClear(TCCR0B, CS00) ;
-        bitClear(TCCR0B, CS01) ;
-        bitClear(TCCR0B, CS02) ;
-        return 0 ;
-    } if (option == 1) {          // T = 1/(16000000/1) = 6.25e-8
-        bitSet(TCCR0B, CS00) ;
-        return 1 ;
-    } if (option == 2) {          // T = 1/(16000000/8) = 5e-7
-        bitSet(TCCR0B, CS01) ;
-        return 8 ;
-    } if (option == 3) {          // T = 1/(16000000/64) = 4e-6
-        bitSet(TCCR0B, CS00) ;
-        bitSet(TCCR0B, CS01) ;
-        return 64 ;
-    } if (option == 4) {          // T = 1/(16000000/256) = 16e-6
-        bitSet(TCCR0B, CS02) ;
-        return 256 ;
-    } if (option == 5) {          // T = 1/(16000000/1024) = 64e-6
-        bitSet(TCCR0B, CS00) ;
-        bitSet(TCCR0B, CS02) ;
-        return 1024 ;
-    } else {
-        return 0 ;
-    }
+   if(option == 0) {
+       bitClear(TCCR0B, CS00) ;
+       bitClear(TCCR0B, CS01) ;
+       bitClear(TCCR0B, CS02) ;
+       return 0 ;
+   } if (option == 1) {          // T = 1/(16000000/1) = 6.25e-8
+       bitSet(TCCR0B, CS00) ;
+       return 1 ;
+   } if (option == 2) {          // T = 1/(16000000/8) = 5e-7
+       bitSet(TCCR0B, CS01) ;
+       return 8 ;
+   } if (option == 3) {          // T = 1/(16000000/64) = 4e-6
+       bitSet(TCCR0B, CS00) ;
+       bitSet(TCCR0B, CS01) ;
+       return 64 ;
+   } if (option == 4) {          // T = 1/(16000000/256) = 16e-6
+       bitSet(TCCR0B, CS02) ;
+       return 256 ;
+   } if (option == 5) {          // T = 1/(16000000/1024) = 64e-6
+       bitSet(TCCR0B, CS00) ;
+       bitSet(TCCR0B, CS02) ;
+       return 1024 ;
+   } else {
+       return 0 ;
+   }
 }
+
 
 void set_tc0_mode(char mode) {
 
-        if(mode == 0) {                // normal, 0xFF, immediate, Max
-            bitClear(TCCR0A, WGM00) ;
-            bitClear(TCCR0A, WGM01) ;
-            bitClear(TCCR0B, WGM02) ;
-        } if (mode == 1) {             // PWM, phase correct, 0xFF, TOP, BOTTOM
-            bitSet(TCCR0A, WGM00) ;
-        } if (mode == 2) {             // CTC, OCRA, immediate, max
-            bitSet(TCCR0A, WGM01) ;
-        } if (mode == 3) {             // Fast PWM, 0xFF, BOTTOM, MAX
-            bitSet(TCCR0A, WGM00) ;
-            bitSet(TCCR0A, WGM01) ;
-        } if (mode == 4) {             // Reserved, -, -, -
-            bitSet(TCCR0B, WGM02) ;
-        } if (mode == 5) {             // PWM, phase correct, OCRA, TOP, BOTTOM
-            bitSet(TCCR0B, WGM02) ;
-            bitSet(TCCR0A, WGM00) ;
-        } if (mode == 6) {             // Reserved, -, -, -
-            bitSet(TCCR0B, WGM02) ;
-            bitSet(TCCR0A, WGM01) ;
-        } if (mode == 7) {             // Fast PWM, OCRA, BOTTOM, TOP
-            bitSet(TCCR0A, WGM00) ;
-            bitSet(TCCR0A, WGM01) ;
-            bitSet(TCCR0B, WGM02) ;
-        }
-        
-    }
-
-
-
-
-void my_delay_us(unsigned long x) {
-
-    int prescalerRet = setPrescaler_tc0(5) ;
-    float Fscaled = 16e6/prescalerRet ;
-    float Tov = ((prescalerRet/16e6) * 256) * 1e6  ;
-
-    unsigned long numOV_max = x/Tov ;
-    unsigned char tcnt0_max = ((float)x - numOV_max * Tov) / Fscaled ;
-   
-    numOV = 0 ;
-    TCNT0 = 0 ;
-    while (numOV < numOV_max - 1) ;
-    setPrescaler_tc0(0) ;
-
-    TCNT0 = 0 ;
-    while (TCNT0 < tcnt0_max - 1) ;
-    setPrescaler_tc0(0) ; 
-}
-
-void my_delay_2s_ctc() {
-    
-    set_tc0_mode(2) ; // select CTC mode
-    OCR0A = 124 ; // set top (instead of default 255)
-    bitSet(TIMSK0, OCIE0A) ; // timer/counter0 output compare match A interrupt
-
-    unsigned long numOV_max = 125 * 2 ;
-
-    numOV = 0 ; // overflow count reset to 0
-    TCNT0 = 0 ; // start timer0
-    setPrescaler_tc0(5) ; // set prescaler to 1024
-    while (numOV < numOV_max) ;  // wait 2s
-    setPrescaler_tc0(0) ; // stop timer 
-}
-
-void my_delay_us_ctc(float x, unsigned char top, char prescaler_option) {
-
-    set_tc0_mode(2) ;
-    OCR0A = top ;
-    bitSet(TIMSK0, OCIE0A) ;
-
-    unsigned long numOV_max = (top + 1) * x ;
-
-    setPrescaler_tc0(prescaler_option) ;
-    numOV = 0 ;
-    TCNT0 = 0 ;
-    
-    while (numOV < numOV_max) ;
-    setPrescaler_tc0(0) ;
-
-}
-
+       if(mode == 0) {                // normal, 0xFF, immediate, Max
+           bitClear(TCCR0A, WGM00) ;
+           bitClear(TCCR0A, WGM01) ;
+           bitClear(TCCR0B, WGM02) ;
+       } if (mode == 1) {             // PWM, phase correct, 0xFF, TOP, BOTTOM
+           bitSet(TCCR0A, WGM00) ;
+       } if (mode == 2) {             // CTC, OCRA, immediate, max
+           bitSet(TCCR0A, WGM01) ;
+       } if (mode == 3) {             // Fast PWM, 0xFF, BOTTOM, MAX
+           bitSet(TCCR0A, WGM00) ;
+           bitSet(TCCR0A, WGM01) ;
+       } if (mode == 4) {             // Reserved, -, -, -
+           bitSet(TCCR0B, WGM02) ;
+       } if (mode == 5) {             // PWM, phase correct, OCRA, TOP, BOTTOM
+           bitSet(TCCR0B, WGM02) ;
+           bitSet(TCCR0A, WGM00) ;
+       } if (mode == 6) {             // Reserved, -, -, -
+           bitSet(TCCR0B, WGM02) ;
+           bitSet(TCCR0A, WGM01) ;
+       } if (mode == 7) {             // Fast PWM, OCRA, BOTTOM, TOP
+           bitSet(TCCR0A, WGM00) ;
+           bitSet(TCCR0A, WGM01) ;
+           bitSet(TCCR0B, WGM02) ;
+       }
+   }
 
